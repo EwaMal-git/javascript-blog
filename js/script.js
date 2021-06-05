226eadda39681116dd656836949ca8acd921dd47
Ewa Malerek <ewa.malerek1985@gmail.com>
2021-06-05 06:05:23 +0200

    Add code to JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   /* remove class 'active' from all article links  */
@@ -22,13 +23,18 @@ for(let activeArticle of activeArticles){
 }
 
   /* get 'href' attribute from the clicked link */
+ 	const articleSelector = clickedElement.getAttribute('href');
+ 	console.log (articleSelector);
+
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add ('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+	const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
