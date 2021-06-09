ad18557ffcf5165c58b077002e9916b0b7366137
Ewa Malerek <ewa.malerek1985@gmail.com>
2021-06-09 20:14:45 +0200

    5.4

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ function titleClickHandler(event){
   /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(const activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  for(const activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
   /* add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
@@ -19,9 +19,9 @@ for(const activeLink of activeLinks){
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
 
-for(const activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
+  for(const activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
@@ -38,16 +38,16 @@ for(const activeArticle of activeArticles){
 
 
 const optArticleSelector = '.post',
-  		optTitleSelector = '.post-title',
-  		optTitleListSelector = '.titles';
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
 function generateTitleLinks(){
-	console.log ('Function was done');
+  console.log ('Function was done');
 
   /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
 
-	titleList.innerHTML= '';
+  titleList.innerHTML= '';
 
   let html = '';
 
@@ -55,7 +55,7 @@ function generateTitleLinks(){
 
   for (const article of articles) {
 
-    const articleId = article.getAttribute('id')
+    const articleId = article.getAttribute('id');
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* create HTML of the link */
@@ -63,7 +63,7 @@ function generateTitleLinks(){
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
     console.log(linkHTML);
 
-     /* insert link into titleList */
+    /* insert link into titleList */
     const titleList = document.querySelector(optTitleListSelector);
     titleList.insertAdjacentHTML('beforeend', linkHTML);
     console.log('titleList: ', titleList);
